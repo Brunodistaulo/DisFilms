2223bac033d79004fc11f2edc9dc89e752f4806e
Bruno Distaulo <0800bruno03@gmail.com>
2024-04-09 00:23:13 -0300

    HW 1 Realizado

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,59 @@
-console.log(tempData);
+function convertirHTML(movie) {
+  const { title, year, director, genre, rate, poster } = movie;
+
+  // const titleMovie = document.createElement('h3');
+  // titleMovie.textContent = title;
+  // titleMovie.id = 'movieTitle'
+
+  // const yearMovie = document.createElement('p');
+  // yearMovie.textContent = year;
+  // yearMovie.classList.add('movieYears');
+
+  // const dtMovie = document.createElement('p');
+  // dtMovie.textContent = director;
+  // dtMovie.classList.add('movieDirector');
+
+  // const gnereMovie = document.createElement('p');
+  // gnereMovie.textContent = genre;
+  // dtMovie.classList.add('movieGnere');
+
+  // const rateMovie = document.createElement('p');
+  // rateMovie.textContent = rate;
+  // rateMovie.classList.add('movieRate');
+  const detalles = document.createElement("p");
+  detalles.textContent = `${title},Director: ${director},
+   Año: ${year},
+    Duración: ${year},
+    Género: ${genre.join(", ")}, 
+    Rate: ${rate};`;
+  detalles.id = "detailsMovie";
+
+  const posterMovie = document.createElement("img");
+  posterMovie.src = poster;
+  posterMovie.classList.add("moviePoster");
+
+  const cardContainer = document.createElement("div");
+  cardContainer.classList.add("containerCard");
+
+  // cardContainer.appendChild(titleMovie);
+  // cardContainer.appendChild(yearMovie);
+  // cardContainer.appendChild(dtMovie);
+  // cardContainer.appendChild(gnereMovie);
+  // cardContainer.appendChild(rateMovie);
+  cardContainer.appendChild(posterMovie);
+  cardContainer.appendChild(detalles);
+
+  return cardContainer;
+}
+
+function MovieAdd() {
+  const cardSection = document.getElementById("cardMovie");
+
+  const newCardSection = tempData.map(convertirHTML);
+
+  newCardSection.forEach((movie) => {
+    cardSection.appendChild(movie);
+  });
+}
+
+MovieAdd();
--- front/scripts/tempData.js
+++ front/scripts/tempData.js
@@ -30,3 +30,40 @@ const tempData = [
       "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg",
   },
 ];
+const tempDataCommingSoon = [
+  {
+    title: "",
+    year: 2024,
+    director: "",
+    genero: [],
+    poster: "",
+  },
+  {
+    title: "",
+    year: 2024,
+    director: "",
+    genero: [],
+    poster: "",
+  },
+  {
+    title: "",
+    year: 2024,
+    director: "",
+    genero: [],
+    poster: "",
+  },
+  {
+    title: "",
+    year: 2024,
+    director: "",
+    genero: [],
+    poster: "",
+  },
+  {
+    title: "",
+    year: 2024,
+    director: "",
+    genero: [],
+    poster: "",
+  },
+];
