5fa8a4aa630adf5e8922eee505d104f5818dc543
Bruno Distaulo <0800bruno03@gmail.com>
2024-04-16 11:31:09 -0300

    hw clase 6 realizado

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,7 +1,15 @@
 const MovieAdd = require("./MovieAdd");
 const axios = require("axios");
 
-axios.get("https://webpt19b.web.app/data/movies.json", (data) => {
-  MovieAdd(data);
-});
+async function getMovie() {
+  try {
+    const response = await axios.get("https://webpt19b.web.app/data/movies.json");
+      const dataMovie = response.data;
+    MovieAdd(dataMovie)
+    
+  } catch (error) {
+    console.log(error.message);
+  }
+}
 
+getMovie();
