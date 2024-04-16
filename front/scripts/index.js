ba2b3d647f67f50cf8e3ba5283b6be0540469146
Bruno Distaulo <0800bruno03@gmail.com>
2024-04-16 10:56:32 -0300

    hw clase 6

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,5 +1,7 @@
 const MovieAdd = require("./MovieAdd");
+const axios = require("axios");
 
-$.get("https://webpt19b.web.app/data/movies.json", (data) => {
+axios.get("https://webpt19b.web.app/data/movies.json", (data) => {
   MovieAdd(data);
 });
+
